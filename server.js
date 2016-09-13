a7506b30b66e73b80b8480ae0097a807fd5d4207
Dylan Conkle <dconkle.dev@gmail.com>
2016-09-12 19:40:53 -0500

    Fixed Environment port

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require('./db/connect');
+var http = require('http');
 var express = require('express');
 var bodyParser = require('body-parser');
 var itemRoutes = require('./routes/item');
 var app = express();
+var server = http.Server(app);
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
@@ -12,6 +14,6 @@ app.use('*', function(req, res) {
   res.status(404).json({message: 'Not Found'});
 });
 
-app.listen(process.env.PORT || 8080);
+server.listen(process.env.PORT || 8080);
 
 exports.app = app;
